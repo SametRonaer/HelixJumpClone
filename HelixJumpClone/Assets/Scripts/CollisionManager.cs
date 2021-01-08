9990c1ba3e60f255b66b6a1c9c1d8247b75d5b34
SametWindows <s.ronaer@hotmail.com>
2021-01-08 03:27:59 +0300

    Dummy design and jumping mechanism added.

--- HelixJumpClone/Assets/Scripts/CollisionManager.cs
+++ HelixJumpClone/Assets/Scripts/CollisionManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class CollisionManager : MonoBehaviour
 {
     [SerializeField] GameObject paintPattern;
+    Rigidbody rb;
+    [SerializeField] float jumpForce = 10f;
+    GameObject paintSplash;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,8 +22,10 @@ public class CollisionManager : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        print(collision.transform.position);
-        Instantiate(paintPattern, transform.GetChild(0).gameObject.transform.position, Quaternion.identity);
+        paintSplash = Instantiate(paintPattern, transform.GetChild(0).gameObject.transform.position, Quaternion.identity);
+        paintSplash.transform.parent = collision.gameObject.transform;
         //Instantiate(paintPattern, collision.transform.position, Quaternion.identity);
+        rb.AddForce(new Vector3(0,jumpForce,0));
+        print(collision.transform.position);
     }
 }
